e6f0e4c2dc8a2ff1ae6500be7db90333ef8a0cbd
YaraDorgham <[yara.dorgham@icloud.com]>
2021-04-14 16:30:45 +0200

    navigation bar modified

--- src/Header.js
+++ src/Header.js
@@ -1,22 +1,27 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom';
-import './Header.css';
-const Header =()=>{
-    const history = useHistory();
+import React from "react";
+import { useHistory } from "react-router-dom";
+import "./Header.css";
+const Header = () => {
+  const history = useHistory();
 
-    const gotoSearch =()=>{
-        history.push('/search')
-    }
-    const gotoPlaying =()=>{
-        history.push('/')
-    }
-    return(
-        <div className="rectangle">
-            <a className="headerSearch" onClick={gotoSearch} >Search</a>
-            <a className="headerPlay" onClick={gotoPlaying} >Playing Now</a>
-        </div>
-    );
-
+  const gotoSearch = () => {
+    history.push("/search");
+  };
+  const gotoPlaying = () => {
+    history.push("/");
+  };
+  return (
+    <div className="rectangle">
+      <div className="centerHeader">
+        <a className="headerSearch" onClick={gotoSearch}>
+          Search
+        </a>
+        <a className="headerPlay" onClick={gotoPlaying}>
+          Playing Now
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
